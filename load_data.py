982f2638e30d4a8d9ba81bd8371abda9bc65e544
Josh Bryden <54197391+JoshyB14@users.noreply.github.com>
2025-02-17 19:09:15 +1100

    Alter load_data to accept Github secret
    added motherduck secret to repo - altered load_data to accept secret as a connection string

--- load_data.py
+++ load_data.py
@@ -1,7 +1,11 @@
 import duckdb
 import os
+
+# get secret from Github repo
+motherduck_token = os.getenv("MOTHERDUCK_TOKEN")
+
 # connect to motherduck database
-con = duckdb.connect('md:?motherduck_token=')
+con = duckdb.connect('md:?motherduck_token=MOTHERDUCK_TOKEN')
 
 # open table builder sql script
 with open('./sql/build_tables.sql', 'r') as build_tables:
