6249ed89bb745989269f65e0765cde47defb0333
Josh Bryden <54197391+JoshyB14@users.noreply.github.com>
2025-02-27 08:33:20 +1100

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -3,23 +3,24 @@ import os
 
 # get secret from Github repo
 motherduck_token = os.getenv("MOTHERDUCK_TOKEN")
-
 # connect to motherduck database
 con = duckdb.connect(f'md:?motherduck_token={motherduck_token}')
 
-# open table builder sql script
+#--------------------------------
+# Run SQL Scripts
+
+# init tables
 with open('./sql/build_tables.sql', 'r') as build_tables:
-    sql_script = build_tables.read()
-    
+    sql_script = build_tables.read() 
 # run in duckdb
 con.sql(sql_script)
 
-# open view builder sql script
+# init views
 with open('./sql/swell_view.sql', 'r') as swell_view:
     sql_script = swell_view.read()
-
 # run in duckdb
 con.sql(sql_script)
+
 
 # close connection
 con.close()
