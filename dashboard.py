e9ed87d3cde6ba73a76d6620ad24281e028968c0
Josh Bryden <54197391+JoshyB14@users.noreply.github.com>
2025-02-27 09:21:05 +1100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,36 +1,28 @@
 import streamlit as st
 import duckdb
-import pandas as pd
-import matplotlib.pyplot as plt
 
 ### Config
 
 # connect to motherduck
 #conn = duckdb.connect('md:?motherduck_token=MOTHERDUCK_TOKEN')
-conn = duckdb.connect('swell_data.duckdb')
 
-### Streamlit 
+conn = duckdb.connect('swell_data.duckdb') # local duckdb for development
+
+### Streamlit
 
 # page config
 st.set_page_config(page_title="Swell Forecast", page_icon="🌊", layout="wide")
 
 # title & desc
 st.title("Swell Forecast Dashboard")
-st.write("Get the latest wave height, period and direction for your favorite surf spots")
 
 # location selection
 # TODO: Implement location selection 
 # selected_location = st.selectbox("Select a location", locations)
 
-data = conn.execute('select * from swell').fetchdf()
+
+# wave direction
+wave_dir = conn.execute("SELECT time, wave_direction from swell;").df()
+st.line_chart(data=wave_dir, x='time', y='wave_direction', x_label='time', y_label='wave_direction')
 
 
-# Plot the Wave Direction vs Time
-st.subheader("Wave Direction Over Time")
-fig, ax = plt.subplots(figsize=(10, 6))
-ax.plot(data['time'], data['wave_direction'], label='Wave Direction (°)', color='orange')
-ax.set_xlabel("Time")
-ax.set_ylabel("Wave Direction (°)")
-ax.set_title("Wave Direction Over Time")
-ax.grid(True)
-st.pyplot(fig)
