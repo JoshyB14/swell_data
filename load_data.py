e75669aa3226ece54bf141a442c64908d20edc14
Josh Bryden <54197391+JoshyB14@users.noreply.github.com>
2025-03-03 22:05:18 +1100

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -1,10 +1,27 @@
 import duckdb
 import os
 
+#--------------------------------
+# Config
+
 # get secret from Github repo
 motherduck_token = os.getenv("MOTHERDUCK_TOKEN")
 # connect to motherduck database
 con = duckdb.connect(f'md:?motherduck_token={motherduck_token}')
+
+
+#--------------------------------
+# Init tables & views
+
+with open('./sql/build_tables.sql', 'r') as build_tables:
+    sql_script = build_tables.read()
+
+con.sql(sql_script)
+
+with open('./sql/swell_view.sql', 'r') as swell_view:
+    sql_script = swell_view.read()
+
+con.sql(sql_script)
 
 #--------------------------------
 # Load data into Motherduck
@@ -12,24 +29,30 @@ con = duckdb.connect(f'md:?motherduck_token={motherduck_token}')
 for file in os.listdir('./landing_zone'):
 
     location_str = file.split('_')[0] # get location name
-    print(location_str)
 
-    with open('./sql/build_tables.sql', 'r') as build_tables:
-        sql_script = build_tables.read() 
+    sql_script = f"""
+    INSERT INTO swell
+    SELECT 
+        {location_str}::TEXT as location,
+        now()::TIMESTAMP as api_call_time,
+        unnest(json_file.hourly.time)::TIMESTAMP AS time,
+        unnest(json_file.hourly.wave_height)::FLOAT AS wave_height,
+        unnest(json_file.hourly.wave_direction)::FLOAT AS wave_direction,
+        unnest(json_file.hourly.wave_period)::FLOAT AS wave_period,
+        unnest(json_file.hourly.swell_wave_height)::FLOAT AS swell_wave_height,
+        unnest(json_file.hourly.swell_wave_direction)::FLOAT AS swell_wave_direction,
+        unnest(json_file.hourly.swell_wave_period)::FLOAT AS swell_wave_period
+    FROM read_json_auto("./landing_zone/{location_str}.json") AS json_file
+    ;
 
-    sql_script.format(location_var=location_str) # format location variable
+    """
 
     # run in duckdb
     con.sql(sql_script)
 
-#--------------------------------
-# Create Motherduck views
 
-# init views
-with open('./sql/swell_view.sql', 'r') as swell_view:
-    sql_script = swell_view.read()
-# run in duckdb
-con.sql(sql_script)
+#--------------------------------
+# Clean up
 
 # close connection
 con.close()
