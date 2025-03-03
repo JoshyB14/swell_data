2eb8db138584ef919fa6c17c63238881e5252177
Josh Bryden <54197391+JoshyB14@users.noreply.github.com>
2025-03-03 21:40:54 +1100

    update get data
    extract locations from db

--- get_swell_data.py
+++ get_swell_data.py
@@ -1,29 +1,43 @@
 import make_api_call as call
+import duckdb
 import json
 import os
 
 
 # openmeteo api
 # https://open-meteo.com/en/docs
-
 # base url
 url = "https://marine-api.open-meteo.com/v1/marine"
 
-# parameters for Freshwater Beach on Sydney's Northern Beaches
+# duckdb setup
+conn = duckdb.connect('locations.duckdb')
+locations = conn.execute("SELECT * FROM locations").fetchdf()
+
+loc = locations['location'].to_list()
+lat = locations['lat'].to_list()
+long = locations['long'].to_list()
+
+# api parameters
 params = {
-	"latitude": -33.784387,
-	"longitude": 151.294499,
-	"hourly": ["wave_height", "wave_direction", "wave_period", "swell_wave_height", "swell_wave_direction", "swell_wave_period"],
-	"timezone": "Australia/Sydney",
-	"forecast_days": 7
+    "latitude": None,
+    "longitude": None,
+    "hourly": ["wave_height", "wave_direction", "wave_period", "swell_wave_height", "swell_wave_direction", "swell_wave_period"],
+    "timezone": "Australia/Sydney",
+    "forecast_days": 7
 }
 
-# calling api function
-freshwater_data = call.make_api_call(url, params)
+# loop through locations and make api call
+for lat, long, loc in zip(lat, long, loc):
+    updated_params = params.copy()
+    updated_params["latitude"] = lat
+    updated_params["longitude"] = long
 
-# save to landing zone
-with open(os.path.join('landing_zone','freshwater_data.json'), 'w') as json_file:
-    json.dump(freshwater_data, json_file, indent=1)
+    # calling api function
+    data = call.make_api_call(url, updated_params)
+
+    # save to landing zone
+    with open(os.path.join('landing_zone',f'{loc}_data.json'), 'w') as json_file:
+        json.dump(data, json_file, indent=1)
 
 
 
