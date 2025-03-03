c0015ff3261cd39ddff2b3b0dc1333e46f17201c
Josh Bryden <54197391+JoshyB14@users.noreply.github.com>
2025-03-03 22:05:16 +1100

    Update build_tables.sql

--- sql/build_tables.sql
+++ sql/build_tables.sql
@@ -13,21 +13,6 @@ CREATE TABLE IF NOT EXISTS swell (
 );
 
 
--- If table exits append data 
-INSERT INTO swell
-SELECT 
-    '{location_var}'::TEXT as location,
-    now()::TIMESTAMP as api_call_time,
-    unnest(json_file.hourly.time)::TIMESTAMP AS time,
-    unnest(json_file.hourly.wave_height)::FLOAT AS wave_height,
-    unnest(json_file.hourly.wave_direction)::FLOAT AS wave_direction,
-    unnest(json_file.hourly.wave_period)::FLOAT AS wave_period,
-    unnest(json_file.hourly.swell_wave_height)::FLOAT AS swell_wave_height,
-    unnest(json_file.hourly.swell_wave_direction)::FLOAT AS swell_wave_direction,
-    unnest(json_file.hourly.swell_wave_period)::FLOAT AS swell_wave_period
-FROM read_json_auto('./landing_zone/{location_var}.json') AS json_file
-;
-
 
 
 
